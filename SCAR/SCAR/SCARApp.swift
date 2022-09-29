0e9a8319bc461261d5eb77bdaf27ed972a26d377
amemariee <awall@csumb.edu>
2022-09-29 12:40:55 -0700

    Hello world app

---
+++ SCAR/SCAR/SCARApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCARApp.swift
+//  SCAR
+//
+//  Created by user228393 on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SCARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
