a483b373c323ab26309888b18b075b0e051634e8
amemariee <awall@csumb.edu>
2022-12-03 21:58:27 -0800

    messge app basic

---
+++ SCARMessage/SCARMessage/SCARMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCARMessageApp.swift
+//  SCARMessage
+//
+//  Created by user228393 on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SCARMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
